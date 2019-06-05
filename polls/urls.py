6bacc730567115f188daf8443ac14fc50a9bf797
Hanuman Huda <hanumanramhuda96@gmail.com>
2019-06-05 18:05:10 +0530

    added antixss API

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('hello',views.hello, name="hello"),
+	path('antixss',views.antixss, name="antixss"),
+	path('xss',views.xss, name="xss"),
     path('', views.index, name='index'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-
-
-def hello(request):
+'''
+XSS Vulnerabilites in hello API
+'''
+def xss(request):
 	id=request.GET.get('id')
 	return HttpResponse("Hello %s" % id)
+
+def antixss(request):
+	id=request.GET.get('id')
+	return render(request,"antixss.html",{'id':id})
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
